84a7685e4b006e37abc553eaf8e6a5c9c9ecabcd
Rishvanam <31215655+Rishvanam@users.noreply.github.com>
2017-08-23 09:06:56 -0700

    Create Program9.java

---
+++ Program9.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+public class Program9
+{
+public static void main(String args[])
+{
+Scanner scan=new Scanner(System.in);
+String str=scan.nextLine();
+int s=str.length();
+System.out.print(s);
+}
+}
