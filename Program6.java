c651f5be77ce41c5aba0fab7bf604e6e972fecbb
Rishvanam <31215655+Rishvanam@users.noreply.github.com>
2017-08-23 08:33:40 -0700

    Create Program6.java

---
+++ Program6.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+public class Program6
+{
+public static void main(String args[])
+{
+Scanner scan=new Scanner(System.in);
+String s="hello";
+for(int i=0;i<5;i++)
+System.out.println(s);
+}
+}
