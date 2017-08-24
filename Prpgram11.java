fab96807766aa8a0d1bca1329a391671e80db5b9
Rishvanam <31215655+Rishvanam@users.noreply.github.com>
2017-08-23 20:52:54 -0700

    Create Prpgram11.java

---
+++ Prpgram11.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+public class Program11
+{
+public static void main(String args[])
+{
+int m,n;
+Scanner scan=new Scanner(System.in);
+m=scan.nextInt();
+n=scan.nextInt();
+double x=Math.pow(m,n);
+System.out.print(x);
+}
+}
