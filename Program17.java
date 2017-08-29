9d13f1eb8010fb04449372df6efe6c823d401ddb
Rishvanam <31215655+Rishvanam@users.noreply.github.com>
2017-08-29 08:21:19 -0700

    Update Program17.java

--- Program17.java
+++ Program17.java
@@ -4,14 +4,20 @@ public class Program17
 {
 public static void main(String args[])
 {
-int m,sum=0;
+int sum=0,n=0;
 Scanner scan=new Scanner(System.in);
 for(int i=1;i<=15;i++)
 {
-m=i%2;
-sum=sum+m;
-i=i/2;
+sum=sum+i;
 }
-System.out.print(sum);
+System.out.println("sum of numbers is"+"="+sum);
+  for(int i=15;i<=45;i++)
+  {
+    if((i%2)!=0)
+    {
+      n=n+i;
+}
+}
+  System.out.println("sum of odd numbers is"+"="+n);
 }
 }
