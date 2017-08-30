e9f45c88c7680c6d1b2d056b78dc414d15073fee
Rishvanam <31215655+Rishvanam@users.noreply.github.com>
2017-08-30 01:24:22 -0700

    Update Program43.java

--- Program43.java
+++ Program43.java
@@ -2,5 +2,11 @@ import java.io.*;
 import java.util.*;
 public class Program43
 {
-Scanner scan=new Scanner(System.in);
-{
+  public static void main(String args[])
+  {
+    Scanner scan=new Scanner(System.in);
+    String s1=scan.nextLine();
+    String S1=scan.nextLine();
+    System.out.print(s1+""+S1);
+  }
+}
