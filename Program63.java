641f7a8b8a8f532b7b7c2e43046b5e877df40e7e
Rishvanam <31215655+Rishvanam@users.noreply.github.com>
2017-09-01 23:11:11 +0530

    Update Program63.java

--- Program63.java
+++ Program63.java
@@ -15,10 +15,9 @@ for(int i=0;i<10;i++)
 a[i]=scan.nextInt();
 }
 Arrays.sort(a);
-}
 for(int i=0;i<10;i++)
 {
-a1=String.valueOf(a[i])+" ";
+a1=a1+String.valueOf(a[i])+" ";
 }
 String a2[]=a1.split(" ");
 for(int j=0;j<10;j++)
@@ -28,15 +27,20 @@ b[j]=scan.nextInt();
 Arrays.sort(b);
 for(int j=0;j<10;j++)
 {
-b1=String.valueOf(b[j])+" ";
+b1=b1+String.valueOf(b[j])+" ";
 }
 String b2[]=b1.split(" ");
 for(int i=0;i<10;i++)
 {
-if(a2[i]=b2[j])
+  a1=a2[i];
+  for(int j=0;j<10;j++)
+  {
+    b1=b2[j];
+if(a1.equals(b1))
 {
 count++;
-System.out.print(a[i]);
+System.out.print(a1);
+}
 }
 }
 if(count==0)
