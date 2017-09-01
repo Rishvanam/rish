c37df0d6e80aaccf3361f5fbb2f400c0bd8e0a60
Rishvanam <31215655+Rishvanam@users.noreply.github.com>
2017-08-31 21:33:24 -0700

    Create Program112c.java

---
+++ Program112c.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+public class Program112c
+{
+public static void main(String[] args)
+{
+Scanner scan=new Scanner(System.in);
+String s=scan.nextLine();
+if(s.equalsIgnoreCase("sun"))
+System.out.print("false");
+else
+System.out.print("true");
+}
+}
