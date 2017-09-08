08ea600b55d3e75b2af7d25193ff55f5ee47c86e
Rishvanam <31215655+Rishvanam@users.noreply.github.com>
2017-09-07 23:39:22 -0700

    Create Program3hunt.java

---
+++ Program3hunt.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+public class Program3hunt
+{
+public static void main(String args[])
+{
+int i;
+Scanner scan=new Scanner(System.in);
+int n=scan.nextInt();
+int a[]=new int[n];
+for(i=0;i<a.length;i++)
+{
+a[i]=scan.nextInt();
+}
+Array.sort(a);
+for(i=0;i<a.length;i++)
+if(a[i]==i)
+System.out.print(a[i]);
+}
+}
