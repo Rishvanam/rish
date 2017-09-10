4f1511a73997eb753ea57cb94c6aaf00b7dd1427
Rishvanam <31215655+Rishvanam@users.noreply.github.com>
2017-09-10 20:51:30 +0530

    Update Program46b.java

--- Program46b.java
+++ Program46b.java
@@ -6,15 +6,11 @@ public static void main(String args[])
 {
 Scanner scan=new Scanner(System.in);
 int a=scan.nextInt();
-int a1=new int[a];
-for(int i=1;i<a1.length;i++)
-{
-a1[i]=scan.nextInt();
-}
-for(int i=1;i<a1.length;i++)
-{
-a=a*i;
-System.out.println("a*i"+=+"a");
+int k=1;
+  for(int i=1;i<=20;i++)
+  {
+    k=a*i;
+    System.out.println(a+"x"+i+"="+k);
 }
 }
 }
