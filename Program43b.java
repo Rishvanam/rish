551b450cf0130dc41977dba0e372e14f5c56d497
Rishvanam <31215655+Rishvanam@users.noreply.github.com>
2017-09-10 18:58:59 +0530

    Create Program43b.java

---
+++ Program43b.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+public class Program43b
+{
+public static void main(String args[])
+{
+Scanner scan=new Scanner(System.in);
+String s1=scan.nextLine();
+String s2=scan.nextLine();
+System.out.print(s1.concat(s2));
+}
+}
