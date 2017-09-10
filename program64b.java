f9c3e4461e0677c9e01c1e59043e263b72243aed
Rishvanam <31215655+Rishvanam@users.noreply.github.com>
2017-09-10 21:51:03 +0530

    Create program64b.java

---
+++ program64b.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+public class Program64b
+{
+public static void main(String args[])
+{
+Scanner scan=new Scanner(System.in);
+String s=scan.nextLine();
+System.out.print(Integer.parseInt(s,2));
+}
+}
