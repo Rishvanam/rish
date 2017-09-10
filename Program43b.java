64b3091aeec36c778265c7b61c24ff4c73f2a703
Rishvanam <31215655+Rishvanam@users.noreply.github.com>
2017-09-10 19:01:14 +0530

    Update Program43b.java

--- Program43b.java
+++ Program43b.java
@@ -7,6 +7,6 @@ public static void main(String args[])
 Scanner scan=new Scanner(System.in);
 String s1=scan.nextLine();
 String s2=scan.nextLine();
-System.out.print(s1.concat(s2));
+System.out.print(s1+s2);
 }
 }
