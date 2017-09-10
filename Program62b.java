fc217bd812be2a47d1e45c7e26b7cbdb336eb255
Rishvanam <31215655+Rishvanam@users.noreply.github.com>
2017-09-10 21:41:27 +0530

    Create Program62b.java

---
+++ Program62b.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+public class Program62b
+{
+public static void main(String args[])
+{
+Scanner scan=new Scanner(System.in);
+int a=scan.nextInt();
+System.out.print(Integer.toBinaryString(a));
+}
+}
