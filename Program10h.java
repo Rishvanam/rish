02acf61d28088f0560bb388c67ffdc39e3372591
Rishvanam <31215655+Rishvanam@users.noreply.github.com>
2017-09-11 09:50:13 +0530

    Create Program10h.java

---
+++ Program10h.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.;
+public class Program10h
+{
+public static void main(String args[])
+{
+Scanner scan=new Scanner(System.in);
+String s1=scan.nextLine();
+String s=scan.nextLine();
+if(s1.contains(s))
+System.out.print("true");
+else
+System.out.print("false");
+}
+}
